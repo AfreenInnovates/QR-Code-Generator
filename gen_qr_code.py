be26aa65e1cdf2992f0bbf28dcc46de1106c0603
Afreen <131018705+AfreenInnovates@users.noreply.github.com>
2023-10-10 14:41:43 +0530

    Update gen_qr_code.py

--- gen_qr_code.py
+++ gen_qr_code.py
@@ -15,22 +15,3 @@ def gen_qrcode(link):
 
 gen_qrcode("")
 
-# import qrcode: This line imports the qrcode module, which is a library in Python for generating QR codes.
-
-# def gen_qrcode(link):: This line defines a function named gen_qrcode that takes one parameter called link. Functions in Python are defined using the def keyword. In this case, the function is defined to generate a QR code for the provided link.
-
-# qr = qrcode.QRCode(: This line creates a new instance of the QRCode class from the qrcode module and assigns it to the variable qr. The QRCode class is used to represent a QR code.
-
-# version = 1: Specifies the version of the QR code. In this case, it's set to version 1.
-# error_correction = qrcode.constants.ERROR_CORRECT_L: Sets the error correction level for the QR code. ERROR_CORRECT_L stands for low error correction.
-# box_size = 20: Specifies the size of each box (module) in the QR code.
-# border = 4: Sets the width of the border around the QR code.
-# qr.add_data(link): This line adds the provided link to the QR code. The add_data method is used to input the data (in this case, the link) that you want to encode into the QR code.
-
-# qr.make(fit=True): This line generates the QR code based on the provided data and the settings specified earlier. The fit=True parameter ensures that the QR code adjusts its size to fit the data properly.
-
-# img = qr.make_image(fill_color="black", black_color="white"): This line creates an image representation of the QR code. The make_image method generates the image, and you can specify the colors of the QR code and the background using the fill_color and black_color parameters, respectively. In this case, the QR code itself is black, and the background is white.
-
-# img.save("qrimg.png"): This line saves the generated QR code image as a PNG file named "qrimg.png" in the current working directory.
-
-# gen_qrcode(""): This line calls the gen_qrcode function with an empty string as the argument. Since the function is designed to accept a link parameter, you might want to replace the empty string with an actual URL or text that you want to encode into the QR code. The provided code will generate a QR code for an empty string, which might not be very useful.
